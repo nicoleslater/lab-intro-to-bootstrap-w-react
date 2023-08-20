aff69791bfafe55a740fd8385c6503d9d8993037
Nicole Slater <nicoleslater@pursuit.org>
2023-08-20 11:29:38 -0400

    Adding Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import postData from "./data/posts.json";
+import Nav from "./components/Nav";
 
 function App() {
   console.log(postData);
   return (
     <main>
-      <button className="btn">hi</button>
+      <Nav />
     </main>
   );
 }
