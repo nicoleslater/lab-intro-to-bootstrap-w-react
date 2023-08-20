18250e62a7a3906fb79d7b81327c1691a402f72b
Nicole Slater <nicoleslater@pursuit.org>
2023-08-20 12:12:30 -0400

    Navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,73 +1,26 @@
 import React from 'react';
 
 const NavBar = () => {
-    return (
-        <nav className="">
-        <div className="">
-          <a className="">Rosier</a>
-          <button
-            className=""
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className=""></span>
-          </button>
-          <div className="" id="navbarSupportedContent">
-            <ul className="">
-              <li className="">
-                <a className="" aria-current="page">
-                  Home
-                </a>
-              </li>
-              <li className="">
-                <a className="">Featured</a>
-              </li>
-              <li className="">
-                <a
-                  className=""
-                  id="navbarDropdown"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Learn More
-                </a>
-                <ul className="" aria-labelledby="navbarDropdown">
-                  <li>
-                    <a className="">About</a>
-                  </li>
-                  <li>
-                    <a className="">Contact</a>
-                  </li>
-                  <li>
-                    <hr className="" />
-                  </li>
-                  <li>
-                    <a className="">Surprise me!</a>
-                  </li>
-                </ul>
-              </li>
-            </ul>
-            <form className="">
-              <input
-                className=""
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-              />
-              <button className="" type="submit">
-                Search
-              </button>
-            </form>
-          </div>
-        </div>
-      </nav>
-    );
-  }
+   console.log(postData.title)
+      
+            return (
+              <div className="nav-box">
+                <header>
+                  <ul className="navbar">
+                    <li className="site-title">
+                      <Link to="/">YouTube</Link>
+                    </li>
+                    <li className="list-item">
+                      <Link to="/">Home</Link>
+                    </li>
+                    <li className="list-item">
+                      <Link to="/about">About</Link>
+                    </li>
+                  </ul>
+                </header>
+              </div>
+            );
+          }
   
 
 export default NavBar;
