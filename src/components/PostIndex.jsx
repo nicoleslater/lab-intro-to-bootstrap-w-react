d76b1021ad3c2403eb14e626c3c728772e5b73f9
Nicole Slater <nicoleslater@pursuit.org>
2023-08-20 12:38:26 -0400

    Setting State

---
+++ src/components/PostIndex.jsx
@@ -0,0 +1,17 @@
+import React, { useEffect } from 'react';
+
+const PostIndex = () => {
+    const [posts, setPosts] = useState([]);
+    const [loadingError, setLoadingError] = useState(false)
+
+    useEffect(() => {
+        
+    })
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default PostIndex;
