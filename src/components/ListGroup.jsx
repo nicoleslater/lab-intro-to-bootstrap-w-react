38eaac93b5e736016b5f15a1fab549f66451fedc
Nicole Slater <nicoleslater@pursuit.org>
2023-08-20 11:57:47 -0400

    ListGroup Component

--- src/components/ListGroup.jsx
+++ src/components/ListGroup.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 
-const ListGroup = ({ postsData }) => {
+const ListGroup = ({ postData }) => {
     <aside className="aside-container">
         <h3>Posts by Location:</h3>
         <ol>
-            {}
+            { postData.map((post) => (
+                post.present ? <li> {post.name}</li> : null
+            ))}
         </ol>
     </aside>
     return (
