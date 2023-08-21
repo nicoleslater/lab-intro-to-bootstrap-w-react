f4318b744f276e69e9c414407f7be4865ead32ef
Nicole Slater <nicoleslater@pursuit.org>
2023-08-21 19:01:05 -0400

    PostListing

--- src/components/PostListing.jsx
+++ src/components/PostListing.jsx
@@ -1,38 +1,73 @@
-import React from 'react';
-import { useState } from 'react';
-import PostInfo from './PostInfo';
-// import {postData} from '../data/posts.json';
 
-const PostListing = ({ postData, togglePostInfo, handleSubmit}) => {
-const [showPostInfo, setShowPostInfo] = useState(false);
+import Post from "./Post";
 
 
-function togglePostInfo(){
-            setShowPostInfo(!showPostInfo);
-        }
-function handleSubmit(event){
-            event.preventDefault();
-        }
+export default function PostListing({postListing}){
+    console.log(postListing)
+
     return (
-        <div>
-        <ul class="list-group">
-        <li class="list-group-item">Post</li>
-        <li class="list-group-item">A second item</li>
-        <li class="list-group-item">A third item</li>
-        <li class="list-group-item">A fourth item</li>
-        <li class="list-group-item">And a fifth one</li>
-      </ul>
-        <div>
-            <button className="toggle-button" onClick={togglePostInfo}>
-                {showPostInfo ? "Go Back..." : "Go to Post..."}
-            </button>
-            {showPostInfo ? (
-                <PostInfo details={postData} handleSubmit={handleSubmit} />
-            ) : null}
-        </div>
-    </div>
-);
+        <>
+        {postListings.map((post, index) => (
+            <Post key={index} post={post} />
+           
+    ))}
+    </>
+    )
 }
 
-export default PostListing;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react';
+// import { useState } from 'react';
+// // import PostInfo from './PostInfo';
+// // import {postData} from '../data/posts.json';
+
+// const PostListing = ({ postData, togglePostInfo, handleSubmit}) => {
+// const [showPostInfo, setShowPostInfo] = useState(false);
+
+
+// function togglePostInfo(){
+//             setShowPostInfo(!showPostInfo);
+//         }
+// function handleSubmit(event){
+//             event.preventDefault();
+//         }
+//     return (
+//         <div>
+//         <ul class="list-group">
+//         <li class="list-group-item">Post</li>
+//         <li class="list-group-item">A second item</li>
+//         <li class="list-group-item">A third item</li>
+//         <li class="list-group-item">A fourth item</li>
+//         <li class="list-group-item">And a fifth one</li>
+//       </ul>
+//         <div>
+//             <button className="toggle-button" onClick={togglePostInfo}>
+//                 {showPostInfo ? "Go Back..." : "Go to Post..."}
+//             </button>
+//             {showPostInfo ? (
+//                 <PostInfo details={postData} handleSubmit={handleSubmit} />
+//             ) : null}
+//         </div>
+//     </div>
+// );
+// }
+
+// export default PostListing;
 
